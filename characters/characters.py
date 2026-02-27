7284a891cac43b215a3f1fdb414b9bb93afd214a
steph-pv <stephshantai@gmail.com>
2026-02-27 07:20:58 -0500

    added characters, locations, stats

---
+++ characters/characters.py
@@ -0,0 +1,46 @@
+Name: Juwaiyrah
+age: 30
+is_parent: True {children: Georgie}
+has_spouse: True {spouse: Abu y}
+industry: logistics
+social: 4
+flags: None
+
+Name: Ali
+age: 17
+is_parent: False
+industry: None
+social: 4
+flags: is_student
+
+name: Liah
+age: 22
+is_parent: False
+industry: philanthropy
+social: 1
+flags: None
+
+name: Georgie
+age: 13
+is_parent: False
+has_family: True {family: mom: Juwaiyrah, dad: Abu y}
+industry: logistics
+social: 4
+flags: is_child
+
+name: Nadia
+age: 47
+is_parent: True {children: Ali, Noor, Mustafa}
+has_spouse: False
+industry: None
+social: 4
+flags:
+
+name: Officer Jay
+age: 40
+is_parent: True {children: Danna}
+has_spouse: False
+industry: transportation
+social: None
+flags: None
+

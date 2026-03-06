2eee13bd5fa60889432755c3a4a51c2746cd3194
steph-pv <stephshantai@gmail.com>
2026-03-06 06:31:21 -0500

    Update social.py

--- src/world/social.py
+++ src/world/social.py
@@ -9,7 +9,38 @@
 # Insult, argue with, grapple with (intellectuals), 
 # ask about school, public transportation, 
 
+from player import*
+from characters import*
 
+Class social:
+    def __init__(self, character):
+        self.name = name
+        self.socialtype = socialtype
+        self.likes = likes
+        self.dislikes = dislikes
+        self.industry = industry
+        self.job = job
+        self.cleanliness = cleanliness
+        character.name = name
+        character.socialtype = socialtype
+        character.likes = likes
+        character.dislikes = dislikes
+        character.industry = industry
+        character.job = job
+        character.cleanliness = cleanliness
 
+    def __str__(self, character):
+        return f"{self.name} started talking to {character.name}"
+    
+    def ask(self, character):
+
+    Questions = {“question”: “Ask about reading.” {“charType”: “1” {“response”: {“I’m a /total/ tabloid junkie.”, “Duh! Don’t we all?”}
+    “charType”: “2” {“response”: “Sure, I do that sometimes, yeah.”}, {“response”: “C’mon man, get real.”},
+    "charType": "3" {"response: {"I thought you’d never ask!"}, {"I can spend hours reading programming documentation."},
+    "believer"
+    #so on and so forth
+    
+    #ask a charType 3 about a book three times in a conversation and receive a book
+    
 class Convo:
     def __init__(self):

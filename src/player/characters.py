62ba8482394513f352eb165b1d3a83e7291edf76
steph-pv <stephshantai@gmail.com>
2026-03-07 23:43:05 -0500

    Update characters.py

--- src/player/characters.py
+++ src/player/characters.py
@@ -1,27 +1,24 @@
-Characters = {
+"Characters": {
     {"name": "Juwaiyrah",
     age": 30,
     "children": "Yusuf",
     "spouse": "Abu Yusuf",
     "industry": "logistics",
-    "social": {"char_type": 4, "likes_actions": ["career", "school", "family", "faith", "favorite_memory" ], 
-        dislikes": ["propose", "make fun of"]}
+    "social": {"char_type": 4, "likes_actions": ["career", "school", "family", "faith", "favorite_memory" ], dislikes": ["propose", "make fun of"]}
     "flags": is_parent, is_student},
     
     {"name": "Ali",
     "age": "17",
     "is_parent": False,
     "industry": None,
-    "social": {"char_type": None, "likes_actions": [], 
-        dislikes": []}
+    "social": {"char_type": None, "likes_actions": [], dislikes": []}
     "flags": is_student},
     
     {"name": "Liah",
     "age": 22,
     "is_parent": False
     "industry": philanthropy,
-    "social": {"char_type": 1, "likes_actions": [], 
-        dislikes": []}
+    "social": {"char_type": 1, "likes_actions": [], dislikes": []},
     "flags": None},
     
     {"name": "Yusuf \'Georgie\'",
@@ -29,40 +26,39 @@ Characters = {
     "is_parent: False,
     "family": {"parents": ["Juwaiyrah", "Abu Yusuf"]},
     "industry": logistics,
-    "social": {"char_type": 4, "likes_actions": [], 
-        dislikes": []}
+    "social": {"char_type": 4, "likes_actions": [], dislikes": []}
     "flags": is_child, is_student},
     
     {"name": "Nadia"
     "age: 47
-    "children": ["Ali", "Noor", "Mustafa"],
-    "social: {"char_type": 4, "likes_actions": [], 
-        dislikes": []},
+    "family": {"children": ["Ali", "Noor", "Mustafa"]},
+    "social: {"char_type": 4, "likes_actions": [], dislikes": []},
     "flags": is_parent},
     
     {"name": "Officer Jay",
     "age": 40,
-    "children": "Danna"
+    "family": {"children": "Danna"},
     "has_spouse": False
     "industry": transportation
-    "social": None
+    "social": {"char_type": None, "likes_actions": [], dislikes": []},
     "flags": is_parent},
     
     {"name": "Danna",
-    "parent": "Officer Jay"
+    "family": {"parent": "Officer Jay"},
     "age": 16,
+    "industry" fashion,
     "flags": is_student},
     
     {"name": "Abu Yusuf",
     "age": 32,
     "industry": philanthropy,
-    "social": 4,
+    "social": {"char_type": 4, "likes_actions": [], dislikes": []},,
     "flags": is_parent}
 
     {"name": "Zoelle",
     "age": 70,
     "industry": sales,
-    "social": 1,
+    "social": {"char_type": 1, "likes_actions": [], dislikes": []},
     "flags": is_parent},
 
 }
